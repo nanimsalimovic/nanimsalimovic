9cf5a2c583e6dd17d46e3cd9cc3c81d4c22ce22e
Salim Boukerma <nanimsalimovic@gmail.com>
2022-06-16 21:38:52 +0100

    First commit of max function

---
+++ max_func.py
@@ -0,0 +1,15 @@
+
+
+def max_func(x, y):
+    return x if x > y else y
+
+
+# The code below will execute only when standalone, the above
+# function still can be imported.
+if __name__ == "__main__":
+    x, y = input("Enter two comma-separated integer values: ").split()
+    assert(x != y), "The given values can't be equal!."
+    try:
+        print(f"The biggest number is {max_func(int(x), int(y))}")
+    except ValueError as e:
+        print(e)
